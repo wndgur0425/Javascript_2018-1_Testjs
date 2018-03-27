62db80c161b2d4334e6273d38e354163c20ffc8f
scheebaren <lifewalk1009@naver.com>
2018-03-27 10:48:06 +0900

    [2018-03-28] Testjs #1

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Cloude Service<br>2018-1';
+var text = 'this is a text.';
+text=10.5;
+var a = 33.7;
+var b = a*text;
+var iceCream = 'chocolate';
+var id = prompt('ID 입력','');
+alert(id + '가 로그인 함.');
